76674d882fc12d80d6c7c0646019480ea78a7431
kyru odioso <kanu_nirvana@hotmail.com>
2022-06-06 08:26:23 -0300

    desktop design

--- js/page-functions.js
+++ js/page-functions.js
@@ -1,4 +1,3 @@
-
 // global vars
 var $wrapper;
 var $body;
@@ -12,8 +11,8 @@ var HH;
 // ---
 
 // the magic begins
-$(document).ready(function() {
-	
+$(document).ready(function () {
+
 	// global vars
 	$wrapper = $('#wrapper');
 	$body = $('body');
@@ -22,26 +21,58 @@ $(document).ready(function() {
 	$window = $(window);
 
 	// ---
+	//DOM elements
+	const modalActive = document.getElementById('modal-active');
+	const modal = document.getElementById('modal');
 
 	// Escribir evento popup aqui
 	console.log("Test");
+	createModal()
 	// global resize
 	globalResize();
 	$window.resize(globalResize);
-	
+
 	// ---
-	
+
 });
+
+//create modal
+function createModal() {
+	const modalGenerator = `<div class="modal-container middle">	
+			                            <div class="modal-header">
+				                            <h2>sign in</h2>
+		                                </div>
+			                                <div class="modal-body">
+				                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro provident modi ad dignissimos vel accusamus a         umquam, architecto inventore dolorum magnam quisquam aliquam ipsa quae odit aut aspernatur doloremque odio.s
+											  </p>
+		                                	</div>
+                                            <div class="modal-footer">
+			                                    <button class="btn btn-transparent" id="close-modal">Close</button>
+		                                    </div>
+	                        </div>`
+	$(".modal").html(modalGenerator)
+	$("#open-modal").on("click", function () {
+		$(".modal-container").show()
+	})
+	$("#close-modal").on("click", function(){
+		$(".modal-container").hide()
+	})
+
+
+
+
+}
+
+
 
 // ---
 
 // global resize
 function globalResize() {
-	
+
 	// set window size
 	WH = $window.height();
 	WW = $window.width();
 	HH = 0;
-	
-}
 
+}
